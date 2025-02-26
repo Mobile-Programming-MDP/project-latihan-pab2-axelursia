1adc9df08eeec15193377a1f1c32bf530f212fd5
axelursia <axeltheowinataursia_2226240117@mhs.mdp.ac.id>
2025-02-26 21:44:27 +0700

    commit 1

---
+++ film/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:film/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+
+}
